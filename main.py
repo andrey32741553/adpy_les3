c038e75ee364629566c86f551eecf50873b6d680
andrey32741553 <65930776+andrey32741553@users.noreply.github.com>
2020-09-13 21:17:34 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from datetime import datetime as dt
+from adpy_les2.countrylinks_class import CountryLinks
+import os
+
+if not os.path.isdir('C:\\logfiles'):
+    os.makedirs('C:\\logfiles')
+
+url = 'https://raw.githubusercontent.com/mledoze/countries/master/countries.json'
+
+
+def path_for_saving(path):
+    def logging(old_function):
+        def new_function():
+            date_time = f'Дата и время создания: {dt.today()}'
+            foo_name = f'Имя функции: {old_function.__name__}'
+            foo_arguments = f'Аргументы функции: {path}'
+            old_foo_result = old_function()
+            with open(path, 'w', encoding='utf-8') as file:
+                file.write(
+                    date_time + '\n' +
+                    '\n' + foo_name + '\n' +
+                    '\n' + foo_arguments + '\n' +
+                    '\n' + 'Результат выполнения: ' + '\n'
+                )
+                for line in old_foo_result:
+                    file.write(line + '\n')
+        return new_function()
+    return logging
+
+
+@path_for_saving('C:\\logfiles\\logfile.txt')
+def decorating_function():
+    result = CountryLinks(url)
+    return result
